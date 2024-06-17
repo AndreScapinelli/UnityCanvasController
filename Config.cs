d25ffe903d9e27a2c0bee866f72695839b406d2c
Zulks <139276203+AndreScapinelli@users.noreply.github.com>
2024-06-17 15:25:57 -0300

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -6,23 +6,9 @@ using Unity.Mathematics;
 using UnityEngine;
 public class Config
 {
+
     public static float InactiveMaxTime
     {
         get { return JSONFile.Configclass.timeout; }
-    }
-
-    public static int PlayerAmountSelectionMaxTime
-    {
-        get { return JSONFile.Configclass.playerAmountSelectionMaxTime;  }
-    }
-
-    public static float ScreenFeedbackTime
-    {
-        get { return JSONFile.Configclass.feedbackScreenTime; }
-    }
-
-    public static float ChallangeMaxTime
-    {
-        get { return JSONFile.Configclass.challangeMaxTime; }
     }
 }
